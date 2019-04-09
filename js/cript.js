2366306ef08c4f2538eb2aeca4c63fdcb3b5b516
douglasproferreira <douglas.ferreira2@estudante.ifms.edu.br>
2019-04-08 22:05:30 -0400

    Adicionando função de tratamento de acentos

--- js/cript.js
+++ js/cript.js
@@ -1,4 +1,8 @@
 var alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var c
+var acento = ["ã", "á", "à", "â", "é", "è", "ê", "í", "î", "ì", "ó", "ô", "ò", "õ", "ú", "ù", "û", "ç"]
+var sAcento = ["a", "a", "a", "a", "e", "e", "e", "i", "i", "i", "o", "o", "o", "o", "u", "u", "u", "c"]
+
 function capturarElemento(ref) {
     const elemento = document.querySelector(ref);
     return elemento;
@@ -11,38 +15,63 @@ criptografar.addEventListener('submit', function (event) {
     event.preventDefault();
 }, false)
 
-
 function verificarChave() {
     let chave = capturarElemento('.chave');
-    let c = chave.value
+    c = chave.value
     if (c > 26 || c <= 0) {
         alert("Chave Inválida - Informe uma chave entre 1 e 26");
         inner.HTML();
     } else {
-        criptografarTexto(c)
+        retirarAcento()
     }
 }
-function criptografarTexto(c) {
-    let ch = parseInt(c);
-    let res = "", resultado = "";
+
+function retirarAcento() {
+    let aux = "", a = "";
     let texto = capturarElemento('.texto').value.toLowerCase();
-    console.log(texto)
-    console.log(texto.length)
+    console.log(texto);
+
     for (let i = 0; i < texto.length; i++) {
         if (texto[i] != " ") {
+            for (let j = 0; j < acento.length; j++) {
+                if (acento[j] == texto[i]) {
+                    aux += sAcento[j]
+                } else {
+                    a = texto[i]
+                }
+            }
+            aux += a
+        } else if (texto[i] == " ") {
+            aux += " "
+        }
+    }
+    console.log(aux);
+    criptografarTexto(aux);
+}
+
+function criptografarTexto(aux) {
+    let ch = parseInt(c);
+    console.log(ch);
+    let res = "", resultado = "";
+    console.log(aux)
+    console.log(aux.length)
+    for (let i = 0; i < aux.length; i++) {
+        if (aux[i] != " ") {
             for (let j = 0; j < alfabeto.length; j++) {
-                if (texto[i] == alfabeto[j]) {
+                if (aux[i] == alfabeto[j]) {
                     let pos = ((j + ch) % 26);
+                    console.log(pos);
                     res = alfabeto[pos];
                     resultado += res;
                 }
             }
-        }
-        else {
+        } else {
             res = " ";
             resultado += res;
         }
     }
+    console.log(resultado);
+    
     mostrarnaTela(resultado);
 }
 
--- js/decript.js
+++ js/decript.js
@@ -1,4 +1,8 @@
 var alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var c
+var acento = ["ã", "á", "à", "â", "é", "è", "ê", "í", "î", "ì", "ó", "ô", "ò", "õ", "ú", "ù", "û", "ç"]
+var sAcento = ["a", "a", "a", "a", "e", "e", "e", "i", "i", "i", "o", "o", "o", "o", "u", "u", "u", "c"]
+
 function capturarElemento(ref) {
     const elemento = document.querySelector(ref);
     return elemento;
@@ -10,27 +14,49 @@ descriptografar.addEventListener('submit', function (event) {
     event.preventDefault();
 }, false)
 
-
 function verificarChave() {
     let chave = capturarElemento('.chave');
-    let c = chave.value
+    c = chave.value
     if (c > 26 || c <= 0) {
         alert("Chave Inválida - Informe uma chave entre 1 e 26");
         inner.HTML();
     } else {
-        descriptografarTexto(c)
+        retirarAcento();
     }
 }
-function descriptografarTexto(c) {
-    let ch = parseInt(c);
-    let res = "", resultado = "";
+
+function retirarAcento() {
+    let aux = "", a = "";
     let texto = capturarElemento('.texto').value.toLowerCase();
-    console.log(texto)
-    console.log(texto.length)
+    console.log(texto);
+
     for (let i = 0; i < texto.length; i++) {
         if (texto[i] != " ") {
+            for (let j = 0; j < acento.length; j++) {
+                if (acento[j] == texto[i]) {
+                    aux += sAcento[j]
+                } else {
+                    a = texto[i]
+                }
+            }
+            aux += a
+        } else if (texto[i] == " "){
+            aux += " "
+        }
+    }
+    console.log(aux);
+    descriptografarTexto(aux);
+}
+
+function descriptografarTexto(aux) {
+    let ch = parseInt(c);
+    let res = "", resultado = "";
+    console.log(aux)
+    console.log(aux.length)
+    for (let i = 0; i < aux.length; i++) {
+        if (aux[i] != " ") {
             for (let j = 0; j < alfabeto.length; j++) {
-                if (texto[i] == alfabeto[j]) {
+                if (aux[i] == alfabeto[j]) {
                     let pos = ((j - ch) % 26);
                     if (pos < 0) {
                         pos = (26 + pos);
@@ -39,7 +65,7 @@ function descriptografarTexto(c) {
                     resultado += res;
                 }
             }
-        } else if (texto[i] == " ") {
+        } else if (aux[i] == " ") {
             res = " ";
             resultado += res;
         }
